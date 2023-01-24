687ec50ff89f7304cfddc8baefb3f644872449cc
alestrab <117847447+alestrab@users.noreply.github.com>
2023-01-24 15:33:33 -0300

    Mario Run using up1

---
+++ Animação/conversor8bit2bit.py
@@ -0,0 +1,51 @@
+import numpy as np
+from tkinter import filedialog
+import pathlib as pat
+
+def gray(r,g,b):
+    gr = round((r+2*g+b)/4,0)
+    if gr==0:
+        return 0
+    elif gr==1:
+        return 1
+    elif gr==2:
+        return 10
+    else:
+        return 11
+
+file_path = filedialog.askopenfilename()
+filename=(pat.Path(file_path).stem)
+if file_path=='':
+    exit()
+else:
+    data = np.loadtxt(file_path,dtype=str)
+i=0
+for x in data:
+    data[i] = x[2:]
+    i=i+1
+video = np.zeros(80,dtype=int)
+i=0 #line
+j=0 #pixel
+for x in data:
+    r = int(x[0:2],2)
+    g = int(x[2:4],2)
+    b = int(x[4:6],2)
+    gr = gray(r,g,b)
+    video[i] = video[i] + gr*(10**(2*j)) #Little endian
+    if j==3:
+        j=0
+        i=i+1
+    else:
+        j=j+1
+with open(filename+'-convertido.bin','wb') as f:
+    np.savetxt(f, video,fmt='%08d')
+
+        
+
+
+
+
+
+
+    
+
